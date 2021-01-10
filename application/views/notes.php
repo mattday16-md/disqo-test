231225a6912e0ef6336fb900181ca02782fe1623
mattday16@gmail.com <mattday16@gmail.com>
2021-01-10 15:06:58 -0600

    Initial

---
+++ application/views/notes.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Notes Site</title>
+
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+
+
+</body>
+</html>
